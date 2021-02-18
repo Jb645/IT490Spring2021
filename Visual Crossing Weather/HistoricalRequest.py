f3d2fec026a947f8c07a724ee9e38a3df9b6a746
Sean Arbelaez <sa2373@njit.edu>
2021-02-18 09:02:16 -0800

    Comments for Script

--- Visual Crossing Weather/HistoricalRequest.py
+++ Visual Crossing Weather/HistoricalRequest.py
@@ -1,8 +1,10 @@
 #! /usr/bin/python3
+
 import requests
 
 url = "https://visual-crossing-weather.p.rapidapi.com/history"
-
+#format better
+#get from query
 querystring = {"startDateTime":"2019-01-01T00:00:00","aggregateHours":"24","location":"Washington,DC,USA","endDateTime":"2019-01-03T00:00:00","unitGroup":"us","dayStartTime":"8:00:00","contentType":"csv","dayEndTime":"17:00:00","shortColumnNames":"0"}
 
 headers = {
