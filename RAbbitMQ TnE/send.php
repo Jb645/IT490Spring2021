dc9bf67c62af70b7ae3ba7ab39a13bdd25d9a5e1
Sean Arbelaez <>
2021-02-24 19:58:15 -0800

    new RabbitMQ producer and consumer

---
+++ RAbbitMQ TnE/send.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('25.14.14.158', 5672, 'test', 'test');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$msg = new AMQPMessage('Hello World!');
+$channel->basic_publish($msg, '', 'hello');
+
+echo " [x] Sent 'Hello World!'\n";
+
+$channel->close();
+$connection->close();
+?>
