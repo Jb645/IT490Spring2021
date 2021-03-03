8c4aa40a10047e517f1e56b032f17fc878914095
Tim <trc22@njit.edu>
2021-03-02 21:27:31 -0500

    Form with that sends login request

--- webserver-files/testRabbitMQClient.php
+++ webserver-files/testRabbitMQClient.php
@@ -1,30 +1,29 @@
 #!/usr/bin/php
 <?php
-ini_set('display_errors', 1);
-error_reporting(-1);
 require_once('rabbitMQLib.inc');
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
+function amqpLoginRequest($username, $password)
 {
-  $msg = $argv[1];
+  $client = new rabbitMQClient("testRabbitMQ.ini","testServer"); 
+  if (isset($argv[1]))
+  {
+    $msg = $argv[1];
+  }
+  else
+  {
+    $msg = "Login request for: " . $username;
+  }
+  $request = array();
+  $request['type'] = "Login";
+  $request['username'] = $username;
+  $request['password'] = $password;
+  $request['message'] = $msg;
+  $response = $client->send_request($request);
+  //$response = $client->publish($request);
+
+  echo "client received response: ".PHP_EOL;
+  echo "\n\n";
 }
-else
-{
-  $msg = "Hello!";
-  echo "message set";
-}
-
-$request = array();
-$request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
-$request['message'] = $msg;
-$response = $client->send_request($request);
-//$response = $client->publish($request);
-
-echo "client received response: ".PHP_EOL;
-//print_r($response);
-echo "\n\n";
-
-echo $argv[0]." END".PHP_EOL;
+$USER = $_GET['username']; 
+$PASS = $_GET['password'];
+amqpLoginRequest($USER, $PASS);
 
