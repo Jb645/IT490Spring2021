c5ca7161a93169877540f63794b9933295338c74
Tim <trc22@njit.edu>
2021-03-02 20:01:50 -0500

    basic webserver stuff

---
+++ webserver-files/index.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+ini_set('display_errors', 1);
+error_reporting(-1);
+require_once('rabbitMQLib.inc');
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "Hello!";
+  echo "message set";
+}
+
+$request = array();
+$request['type'] = "Login";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+
