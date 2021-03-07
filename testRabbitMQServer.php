31728826a3656051fada795d7ad2137f8ce12762
Tim <trc22@njit.edu>
2021-03-06 21:52:31 -0500

    moved server functions into separate file for organization

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -4,57 +4,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('logger.php');
-
-function doLogin($username,$password)
-{
-    // lookup username in databas
-    // check password
-    return true;
-    //return false if not valid
-}
-
-function createAccount($usernmane, $password)
-{
-  //Insert account info into DB
-  return true; //Return true on success
-}
-
-function getWeather($location)
-{
-  $output = shell_exec("python3 RAbbitMQ-TnE/WeatherPONG.py ". $location);
-  if(!isset ($output))
-  {
-    logData("No data recieved from weather API");	  
-  }
-  $output = preg_split('/\r\n|\n/', $output);
-  $weather = array((float) filter_var( $output[0], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION), substr($output[3],15));
-  LogData("Temp: " . $weather[0] ." | Desc: ". $weather[1] ."\n");
-  return $weather;
-}
-
-function insertResults($request)
-{
-  //SQL insert results into DB
-  return true; //Return true on success
-}
-
-function getFriends($username)
-{
-  //SQL retrieve list of friends from DB
-  return true; //Return true on success
-}
-
-function addFriend($username, $target)
-{
-  //SQL insert target into username's friends list
-  return true; //Return true on success
-}
-
-function rmFriend($username, $target)
-{
-  //SQL remove target from username's friend's list
-  return true; //Return true on success
-}
+require_once('serverFunctions.php');
 
 function requestProcessor($request)
 {
