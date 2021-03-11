9f23d76215551ecc350e56cb7d5a049fc0675149
Matt Belo <mab222@njit.edu>
2021-03-11 11:13:18 -0500

    Finishing up Database connection within Server Functions File

--- serverFunctions.php
+++ serverFunctions.php
@@ -20,8 +20,9 @@ function doLogin($username,$password)
 {
     // lookup username in databas
     // check password
+    $mydb = dbConnect();
     $sql = "SELECT userName FROM Users WHERE username = '$username' and password = '$password'";
-    $result = mysqli_query($db, $sql);
+    $result = mysqli_query($mydb, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $active = $row['active'];
     $count = mysqli_num_rows($result);
@@ -29,6 +30,7 @@ function doLogin($username,$password)
         return true;
     }
 
+    $mydb->close();
     //return false if not valid
 }
 
@@ -87,17 +89,81 @@ function getWeatherHistory($location, $date)
   return $weather;
 }
 
-function insertResults($request)
+function scoreTableChecker($id){
+      $mydb = dbconnect();
+      $sql = "SELECT * FROM scores WHERE id=$id";
+      if($mydb->query($sql) != TRUE){
+         $newRowSql = "INSERT INTO scores (id) VALUES ($id)";
+         $mydb->query($newRowSql);
+      }
+      $mydb->close();
+}
+
+function insertResults($request, $id)
 {
   //SQL insert results into DB
-  $winner = $request['winner'];
-  $loser = $request['loser'];
+  
+  switch($request){
+        case "winner":
+           $winner = $request['winner'];  //insert winner into users
+           $mydb = dbConnect();
+           $sqlWinnerSearch = "SELECT wins FROM users WHERE id=$id";
+           $winsSearch = mysql_query($mydb, $sqlWinnerSearch);
+	   if ($mydb->query($winsSearch) == TRUE){
+               $newWins = $winsSearch + 1;
+	       $sqlWins = "UPDATE users SET wins=$newWins WHERE id=$id";
+	       $mydb->query($sqlWins);
+	   }
+           else{
+                $sqlEmptyWins ="UPDATE users SET wins=1 WHERE id=$id";
+                $mydb->query($sqlEmptyWins);
+	   }
+           $mydb->close();
+           break;
+        case "loser":
+           $loser = $request["loser"];
+           $mydb = dbConnect();
+           $sqLoserSearch = "SELECT losses FROM users WHERE id=$id";
+           $lossesSearch = mysql_query($mydb, $sqlLoserSearch);
+           if ($mydb->query($lossesSearch) == TRUE){
+               $newLosses = $lossesSearch + 1;
+               $sqlLosses = "UPDATE users SET losses=$newLosses WHERE id=$id";
+               $mydb->query($sqlLosses);
+           }
+           else{
+                $sqlEmptyLosses = "UPDATE users SET losses=1 WHERE id=$id";
+                $mydb->query($sqlEmptyLosses);
+           }
+           $mydb->close();
+           break;
+         case 'wscore':
+           $winscore = $request['wscore'];  //insert winner into users
+	   $mydb = dbConnect();
+	   scoreTableChecker($id);
+           $sqlWinScoreSearch = "SELECT wins FROM scores WHERE id=$id";
+           $winsSearch = mysql_query($mydb, $sqlWinnerSearch);
+           if ($mydb->query($winsSearch) == TRUE){
+               $newWins = $winsSearch + 1;
+               $sqlWins = "INSERT INTO users (wins) VALUES ($newWins) WHERE id=$id";
+               $mydb->query($sqlWins);
+           }
+           else{
+                $sqlEmptyWins = "INSERT INTO users (wins) VALUES (1) WHERE id=$id";
+                $mydb->query($sqlEmptyWins);
+           }
+           $mydb->close();
+           break;
+
   $winscore = $request['wscore'];
   $loserscore = $request['lscore'];
   $wweather = $request['wweather'];//Weather cond for winnner
   $lweather = $request['lweather'];//Weather cond for loser
 
+  $mydb = dbConnect();
+  //Insert account info into DB
+  $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
   return true; //Return true on success
+  }
 }
 
 function getFriends($username)
