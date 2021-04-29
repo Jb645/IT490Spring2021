e6d2f0fe518972429dcc42922fa381f0d646b782
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-04-29 12:59:08 -0400

    Updated the loggedIn.php

--- webserver-files/loggedIn.php
+++ webserver-files/loggedIn.php
@@ -1,30 +1,42 @@
 <?php
 session_start();
 ?>
-<html>
+<html lang="en">
 	<head>
-		<title>Successful Login</title>
+		<meta charset="UTF-8">
 		<link rel="stylesheet" href="WebpageStyle.css">
-		<link rel="stylesheet" href="bootstrap-5/css/bootstrap.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+			  rel="stylesheet"
+			  integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+			  crossorigin="anonymous">
+		<title>Successful Login</title>
 	</head>
-	<body>
-	
-	<nav class="nav">
+	<body class="body">
+
+	<nav>
 		<ul>
 			<li><a href = "leaderboards.php"> Leaderboards </a></li>
 			<li><a href = "matchmaking.php"> Suggest opponent </a></li>
-			<li><a href = "profile.php"> View profile </a></li>
-			<li><a href = "friends.php"> View friends </a></li>
+			<li><a href = "#"> Store </a></li>
 			<li><a href = "chat.php"> Chat </a></li>
-			<li><a href = "shop.php"> Shop </a></li>
+			<li><a href="#"> Profile </a>
+				<ul>
+					<li><a href="#">Achievements</a></li>
+					<li><a href="Account.html">Account</a></li>
+					<li><a href="friends.php">View Friends</a></li>
+				</ul>
+			</li>
 			<li><a href = "exit.php"> Exit </a></li>
+
 		</ul>
+		<br>
 		<br>
 		<p class = "Welcome">Welcome: <?php echo $_SESSION['login']; ?></p> <br>
 	</nav>
 
-
-
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+			crossorigin="anonymous"></script>
 	</body>
 </html>
 <?php 
