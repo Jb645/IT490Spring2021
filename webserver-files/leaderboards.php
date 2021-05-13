e628cf2571f11826e1f80bf99bb7b42bdfee1601
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-05-12 20:52:48 -0400

    Fixed Shop_2

--- webserver-files/leaderboards.php
+++ webserver-files/leaderboards.php
@@ -44,35 +44,38 @@ session_start();
 	</nav>
 		<p id="Welcome">Leaderboards:</p>
 	</header>
+    <main class="trueMain">
+        <?php
+           ini_set('display_errors',1);
+           ini_set('display_startup_errors', 1);
+           error_reporting(E_ALL ^ E_DEPRECATED);
 
+           require("testRabbitMQClient.php");
+
+           if(isset ($_SESSION['login']) && !empty($_SESSION['login']))
+           {
+            echo nl2br("Session is good\n");
+            $users = amqpLeaderboard();
+            echo nl2br("\n\nTop 10: \n");
+            echo nl2br("\nName | Wins | Losses\n--------------------------------");
+            $count = 0;
+            while($count < 10)
+            {
+                echo nl2br("\n" . ($count + 1) . ": $users[$count]");
+                $count ++;
+            }
+           }
+           else
+           {
+            header("Location: index.php");
+           }
+       ?>
+   </main>
 	<footer>
 		<a href = "loggedIn.php"> Back</a> <br>
 	</footer>
 
 	</body>
 </html>
-<?php 
-ini_set('display_errors',1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL ^ E_DEPRECATED);
 
-require("testRabbitMQClient.php");
 
-if(isset ($_SESSION['login']) && !empty($_SESSION['login']))
-{
-	echo nl2br("Session is good\n");
-	$users = amqpLeaderboard();
-	echo nl2br("\n\nTop 10: \n");
-	echo nl2br("\nName | Wins | Losses\n--------------------------------");
-	$count = 0;
-	while($count < 10)
-	{
-		echo nl2br("\n" . ($count + 1) . ": $users[$count]");
-		$count ++;
-	}
-}
-else
-{
-	header("Location: index.php");
-}	
-?> 
